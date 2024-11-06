b6d2c1c851c82827d6aa8ca045bcf668dcff7ad3
Freelancer0708 <freelancernoaccont@gmail.com>
2024-11-06 10:11:10 +0900

    first commit

---
+++ slider.js
@@ -0,0 +1,184 @@
+window.addEventListener("load", () => {
+  const classNames = {
+    slider: "custom-slider",
+    sliderInner: "custom-slider-inner",
+    slide: "custom-slide",
+  };
+  // カッコ内に書かないと、letやconstはグローバルスコープ(外部アクセスが可能)になってしまうため、ここに記載してます。
+  // varはどこに書いてもグローバルスコープなので、なるべく使わないようにする。
+  let sliders = document.querySelectorAll(`.${classNames.slider}`);
+  sliders.forEach((slider) => {
+    // スライドの初期設定
+    const slideitems = Array.from(slider.children);
+    slideitems.forEach((slideitem) => {
+      let slideWapper = document.createElement("div");
+      slideWapper.classList.add(classNames.slide);
+      slideWapper.appendChild(slideitem);
+      slider.appendChild(slideWapper);
+    });
+    let sliderWapper = document.createElement("div");
+    sliderWapper.classList.add(classNames.sliderInner);
+    while (slider.firstChild) {
+      sliderWapper.appendChild(slider.firstChild);
+    }
+    slider.appendChild(sliderWapper);
+
+    // スライドの挙動の初期値
+    let startX = 0;
+    let targetX = 0;
+    let currentX = 0;
+    let translateX = 0;
+    let currentSlide = 1;
+    let isDragging = false;
+    let isInfiniteSliderActivePrev = false;
+    let isInfiniteSliderActiveNext = false;
+    let infiniteX = 0;
+    let infiniteLength = 0;
+    
+
+    // スライドの作成後の設定
+    const sliderInner = slider.querySelector(`.${classNames.sliderInner}`);
+    const slides = sliderInner.querySelectorAll(`.${classNames.slide}`);
+
+    // スライドの1画面の表示枚数
+    let slidesToShow = parseInt(slider.getAttribute("data-show")) || 1;
+    if(slidesToShow <= 0) {slidesToShow = 1;}
+    if(slidesToShow > slides.length) {slidesToShow = slides.length;}
+    const oneslideWidth = sliderInner.offsetWidth / slidesToShow;
+    slides.forEach((slideitem) => {
+      slideitem.style.width = `${oneslideWidth}px`;
+    });
+
+    // 無限スライドの設定
+    const isInfinite = slider.getAttribute("data-infinite") === "true";
+    // slider-inner の幅をスライド数で割り、スライドの移動幅を計算
+    const sliderOffsetWidth = sliderInner.offsetWidth;
+    const slideWidth = sliderInner.offsetWidth / slides.length;
+    infiniteX = slideWidth * -2;
+    if(isInfinite) {
+      const sliderInnerItems = Array.from(sliderInner.children);
+      const firstCopies = sliderInnerItems.slice(0, 2).map(slide => slide.cloneNode(true));
+      const lastCopies = sliderInnerItems.slice(-2).map(slide => slide.cloneNode(true));
+      firstCopies.forEach(copy => sliderInner.appendChild(copy));
+      lastCopies.reverse().forEach(copy => sliderInner.insertBefore(copy, sliderInner.firstChild));
+      currentSlide = 3;
+      infiniteLength = 2;
+      sliderInner.style.transform = `translateX(${infiniteX}px)`;
+    }
+
+    // スライドの挙動
+    // マウスをクリックしている間に起こる関数
+    function onMouseMove(e) {
+      // 移動距離(deltaX) = クリックしながら移動したマウスの位置(e.clientX) - クリックし始めた位置(startX)
+      const deltaX = e.clientX - startX;
+      // 移動距離の絶対値(Math.abs(deltaX)) が、スライド幅の4/5(sliderInner.offsetWidth * 4 / 5) より大きい時
+      if (Math.abs(deltaX) >= slider.offsetWidth * 2 / 5) {
+        // 現在のスライドの番号(currentSlide) を更新
+        currentSlide -= Math.sign(deltaX);
+        // 現在のスライドの番号(currentSlide) が 1以上 スライドの枚数(slides.length) 以下の時
+        // if (!isInfinite) {
+          if (currentSlide >= 1 && currentSlide <= slides.length) {
+            // スライダーを1枚分移動させる(targetX) += 1枚のスライド幅(slideWidth) * 移動させる方向(Math.sign(deltaX))
+            targetX += slideWidth * Math.sign(deltaX);
+            // スライドが移動したら、初期位置を、現在のマウスの位置に更新する(startX = e.clientX)
+            startX = e.clientX;
+          
+          } else if (isInfinite && currentSlide < 3) {
+            currentSlide = 3;
+          } else if (currentSlide > slides.length + 2) {
+            currentSlide = slides.length + 2;
+          // 現在のスライドの番号(currentSlide) が 1より小さい時
+          } else if (currentSlide < 1) {
+            // 現在のスライドの番号(currentSlide) を1に設定
+            currentSlide = 1;
+          // 現在のスライドの番号(currentSlide) が スライドの枚数(slides.length) より大きい時
+          } else if (currentSlide > slides.length) {
+            // 現在のスライドの番号(currentSlide) をスライドの枚数(slides.length) に設定
+            currentSlide = slides.length;
+          }
+        // } else {
+          // targetX += slideWidth * Math.sign(deltaX);
+          // startX = e.clientX;
+          // if (currentSlide < 1 + infiniteLength && Math.sign(deltaX) > 0) {
+          //   targetX = slideWidth;
+          //   isInfiniteSliderActivePrev = true;
+          // } else if (currentSlide > slides.length + infiniteLength && Math.sign(deltaX) < 0) {
+          //   targetX = -sliderOffsetWidth + oneslideWidth * 2;
+          //   isInfiniteSliderActiveNext = true;
+          // }
+        // }
+      }
+    }
+    // アニメーション関数
+    function animate() {
+      const sliderInnerItems = Array.from(sliderInner.children);
+      // しきい値(閾値)の設定
+      const threshold = 5;
+      // 現在のスライド位置(currentX) が 目標の位置(targetX) まで 閾値(threshold) よりも小さい時
+      if (Math.abs(targetX - currentX) < threshold) {
+        // 現在のスライド位置(currentX) を 目標の位置(targetX) まで 動かす
+        currentX = targetX;
+        // スライド位置(currentX) を 動かす
+        translateX = currentX + infiniteX;
+        sliderInner.style.transform = `translateX(${translateX}px)`;
+        console.log("currentX:"+ currentX);
+        console.log("infiniteX:"+ infiniteX);
+        console.log("currentSlide:"+ currentSlide);
+        console.log("translateX:"+ translateX);
+        console.log("oneslideWidth:"+ -oneslideWidth);
+        if(isInfinite && translateX > -oneslideWidth * 2) {
+          const prevSlide = sliderInnerItems.slice(-1).map(slide => slide.cloneNode(true));
+          prevSlide.reverse().forEach(copy => sliderInner.insertBefore(copy, sliderInner.firstChild));
+          sliderInner.lastElementChild.remove();
+          currentSlide = 3;
+          translateX -= oneslideWidth;
+          sliderInner.style.transform = `translateX(${translateX}px)`;
+          console.log("Prev currentX:"+ currentX);
+          console.log("Prev infiniteX:"+ infiniteX);
+          console.log("Prev currentSlide:"+ currentSlide);
+          console.log("Prev translateX:"+ translateX);
+          console.log("Prev oneslideWidth:"+ -oneslideWidth);
+        } else if (isInfinite && translateX < infiniteX - sliderOffsetWidth + slideWidth) {
+          const nextSlide = sliderInnerItems.slice(0, 1).map(slide => slide.cloneNode(true));
+          nextSlide.forEach(copy => sliderInner.appendChild(copy));
+          sliderInner.firstElementChild.remove();
+          targetX = infiniteX - sliderOffsetWidth + slideWidth;
+          currentX = infiniteX - sliderOffsetWidth + slideWidth;
+          translateX = infiniteX - sliderOffsetWidth + slideWidth;
+          sliderInner.style.transform = `translateX(${translateX}px)`;
+        }
+        // クリックが継続しているか判定する
+        if (!isDragging) return;
+      } else {
+        // 現在のスライド位置(currentX) を 目標の位置(targetX) まで 少しずつ動かす
+        currentX += (targetX - currentX) * 0.12;
+        // スライド位置(currentX) を 動かす
+        translateX = currentX + infiniteX;
+        sliderInner.style.transform = `translateX(${translateX}px)`;
+      }
+      // 次の画面更新でanimateを再度実行
+      requestAnimationFrame(animate);
+    }
+
+    // スライダー(.slider-inner)クリック時
+    slider.addEventListener("mousedown", (e) => {
+      // 最初の位置にマウスの位置を設定
+      startX = e.clientX;
+      isDragging = true;
+      // 画面更新でanimateを実行
+      requestAnimationFrame(animate);
+      // マウスが動いたらonMouseMoveを実行
+      document.addEventListener("mousemove", onMouseMove);
+      // クリックを離した時
+      document.addEventListener("mouseup", onMouseUp);
+    });
+
+    function onMouseUp() {
+      isDragging = false;
+      // onMouseMoveを停止
+      document.removeEventListener("mousemove", onMouseMove);
+      // onMouseUpを停止
+      document.removeEventListener("mouseup", onMouseUp);
+    }
+  });
+});
