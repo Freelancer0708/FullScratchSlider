728c1de8bd18f1fcde97da4069a6f8eddd50d8ac
Freelancer0708 <freelancernoaccont@gmail.com>
2024-11-16 23:51:55 +0900

    fix js

--- slider.js
+++ slider.js
@@ -1,30 +1,120 @@
-import { Show } from './show.js';
-import { InfiniteIndex, InfiniteSlide, InfiniteLast } from './infinite.js';
+const classNames = {
+  slider: "custom-slider",
+  sliderInner: "custom-slider-inner",
+  slide: "custom-slide",
+};
+
+// スライドの初期設定
+function InitializeSlider(slider) {
+  const slideitems = Array.from(slider.children);
+  slideitems.forEach((slideitem) => {
+    let slideWapper = document.createElement("div");
+    slideWapper.classList.add(classNames.slide);
+    slideWapper.appendChild(slideitem);
+    slider.appendChild(slideWapper);
+  });
+  let sliderWapper = document.createElement("div");
+  sliderWapper.classList.add(classNames.sliderInner);
+  while (slider.firstChild) {
+    sliderWapper.appendChild(slider.firstChild);
+  }
+  slider.appendChild(sliderWapper);
+};
+
+// 無限スライドの設定
+// 無限スライドの初期設定
+function InfiniteIndex(isInfinite,oneslideWidth,sliderInner) {
+  console.log("InfiniteIndex:" + isInfinite);
+  const slides = sliderInner.children;
+  // 最初の要素を複製
+  const firstClone = slides[0].cloneNode(true);
+  // 最後の要素を複製
+  const lastClone = slides[slides.length - 1].cloneNode(true);
+  // 最後の要素に追加
+  sliderInner.appendChild(firstClone);
+  // 最初の要素に追加
+  sliderInner.insertBefore(lastClone, slides[0]);
+  const currentSlide = 2;
+  let targetX = -oneslideWidth;
+  let currentX = -oneslideWidth;
+  let translateX = -oneslideWidth;
+  sliderInner.style.transform = `translateX(${translateX}px)`;
+  return {currentSlide,targetX,currentX,translateX};
+};
+// 無限スライドの挙動の設定
+function InfiniteSlide(isInfinite,targetX,slideWidth,deltaX,startX,e) {
+  console.log("InfiniteSlide:" + isInfinite);
+  // スライダーを1枚分移動させる(targetX) += 1枚のスライド幅(slideWidth) * 移動させる方向(Math.sign(deltaX))
+  targetX += slideWidth * Math.sign(deltaX);
+  // スライドが移動したら、初期位置を、現在のマウスの位置に更新する(startX = e.clientX)
+  startX = e.clientX;
+  return {targetX,startX};
+};
+// 無限スライドの作成後の設定
+function InfiniteLast(isInfinite,currentSlide,currentX,targetX,translateX,slider,sliderInner,slideWidth) {
+  console.log("InfiniteLast:" + isInfinite);
+  const infiniteSliderInner = slider.querySelector(`.${classNames.sliderInner}`);
+  const infiniteSlides = infiniteSliderInner.querySelectorAll(`.${classNames.slide}`);
+  const infiniteSlids = infiniteSliderInner.children;
+  // 最初の要素を複製
+  const firstClone = infiniteSlids[2].cloneNode(true);
+  // 最後の要素を複製
+  const lastClone = infiniteSlids[infiniteSlids.length - 3].cloneNode(true);
+  // 現在のスライドの番号(currentSlide) が 1より小さい時
+  if (currentSlide < 2) {
+    // 現在のスライドの番号(currentSlide) を1に設定
+    currentSlide = 2;
+    infiniteSliderInner.removeChild(infiniteSlides[infiniteSlides.length - 1]);
+    infiniteSliderInner.insertBefore(lastClone, infiniteSlides[0]);
+    targetX -= slideWidth;
+    currentX = targetX;
+    translateX = currentX;
+    sliderInner.style.transform = `translateX(${translateX}px)`;
+  // 現在のスライドの番号(currentSlide) が スライドの枚数(slides.length) より大きい時
+  } else if (currentSlide >= infiniteSlides.length - 1) {
+    // 現在のスライドの番号(currentSlide) をスライドの枚数(slides.length) に設定
+    currentSlide = infiniteSlides.length - 2;
+    infiniteSliderInner.removeChild(infiniteSlides[0]);
+    infiniteSliderInner.appendChild(firstClone);
+    targetX += slideWidth;
+    currentX = targetX;
+    translateX = currentX;
+    sliderInner.style.transform = `translateX(${translateX}px)`;
+  }
+  if (translateX > -slideWidth) {
+    targetX = -slideWidth;
+    currentX = targetX;
+    translateX = currentX;
+    sliderInner.style.transform = `translateX(${translateX}px)`;
+  }
+  if (translateX < -slideWidth * (infiniteSlides.length - 3)) {
+    targetX = -slideWidth * (infiniteSlides.length - 3);
+    currentX = targetX;
+    translateX = currentX;
+    sliderInner.style.transform = `translateX(${translateX}px)`;
+  }
+  return {currentSlide,targetX,currentX,translateX};
+};
+
+// スライドの1画面の表示枚数の設定
+function Show(slidesToShow,slides) {
+  // slidesToShowが0以下は1に変更
+  if(slidesToShow <= 0) {slidesToShow = 1;}
+  // slidesToShowがスライドの数(slides.length)以上の時、スライドの数(slides.length)に変更
+  if(slidesToShow > slides.length) {slidesToShow = slides.length;}
+  return slidesToShow; // slidesToShowを返す
+};
+
+
 
 window.addEventListener("load", () => {
-  const classNames = {
-    slider: "custom-slider",
-    sliderInner: "custom-slider-inner",
-    slide: "custom-slide",
-  };
   // カッコ内に書かないと、letやconstはグローバルスコープ(外部アクセスが可能)になってしまうため、ここに記載してます。
   // varはどこに書いてもグローバルスコープなので、なるべく使わないようにする。
   let sliders = document.querySelectorAll(`.${classNames.slider}`);
   sliders.forEach((slider) => {
+
     // スライドの初期設定
-    const slideitems = Array.from(slider.children);
-    slideitems.forEach((slideitem) => {
-      let slideWapper = document.createElement("div");
-      slideWapper.classList.add(classNames.slide);
-      slideWapper.appendChild(slideitem);
-      slider.appendChild(slideWapper);
-    });
-    let sliderWapper = document.createElement("div");
-    sliderWapper.classList.add(classNames.sliderInner);
-    while (slider.firstChild) {
-      sliderWapper.appendChild(slider.firstChild);
-    }
-    slider.appendChild(sliderWapper);
+    InitializeSlider(slider);
 
     // スライドの挙動の初期値
     let startX = 0;
